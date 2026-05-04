67b9b4f9e6fcebaa175603eda67b1f35cd73379b
shiljastephen <shiljastephen@gmail.com>
2026-05-04 17:39:11 +0530

    Updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,9 @@
 import "./Skills.css";
 import { FaPython, FaReact, FaHtml5, FaCss3Alt, FaNodeJs, FaGithub, FaGit} from "react-icons/fa";
-import { SiDjango, SiJavascript, SiAngular } from "react-icons/si";
-import { SiMysql, SiPostgresql, SiMongodb, SiSqlite, SiPostman, SiExpress } from "react-icons/si";
+import { SiDjango, SiJavascript, SiAngular, SiBootstrap, SiJquery, SiVercel} from "react-icons/si";
+import { SiMysql, SiPostgresql, SiMongodb, SiSqlite, SiPostman} from "react-icons/si";
 import { TbApi } from "react-icons/tb";
+import { VscCode } from "react-icons/vsc";
 
 const skillsData = [
   {
@@ -22,6 +23,8 @@ const skillsData = [
       { name: "Angular", icon: <SiAngular />, level: "70%" },
       { name: "HTML", icon: <FaHtml5 />, level: "85%" },
       { name: "CSS", icon: <FaCss3Alt />, level: "80%" },
+      { name: "Bootstrap", icon: <SiBootstrap />, level: "75%" },
+      { name: "jQuery", icon: <SiJquery />, level: "65%" },
     ],
   },
   {
@@ -31,14 +34,16 @@ const skillsData = [
       { name: "PostgreSQL", icon: <SiPostgresql />, level: "70%" },
       { name: "MongoDB", icon: <SiMongodb />, level: "80%" },
       { name: "SQLite", icon: <SiSqlite />, level: "90%" },
-    ]
+    ],
   },
   {
     title: "Tools",
     items: [
-      { name: "Git", icon: <FaGit />, level: "75%" },
-      { name: "GitHub", icon: <FaGithub />, level: "80%" },
-      { name: "Postman", icon: <SiPostman />, level: "70%" },
+      { name: "Git", icon: <FaGit />, level: "85%" },
+      { name: "GitHub", icon: <FaGithub />, level: "90%" },
+      { name: "Postman", icon: <SiPostman />, level: "80%" },
+      { name: "VS Code", icon: <VscCode />, level: "85%" },
+      { name: "Vercel", icon: <SiVercel />, level: "75%" },
     ],
   },
 ];
