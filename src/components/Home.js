8266a263b23c299bd072b5f39d3dfdf02ac69d12
shiljastephen <shiljastephen@gmail.com>
2026-05-04 17:12:42 +0530

    Updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,10 @@ function Hero() {
 
   return (
     <section className="hero" data-aos="fade-up">
-      <h1>Hi, I'm Shilja</h1>
-      <h2>Python & React Developer</h2>
-      <p>I build scalable full-stack web applications using Django and
-        React, focusing on performance, clean architecture, and user-friendly design.</p>
+      <h1>Hi, I'm Shilja 👋</h1>
+      <h2>Full-Stack Developer (Django & React)</h2>
+      <p>I create modern, scalable web applications using Django and React, 
+        focused on performance, clean architecture, and seamless user experience.</p>
 
       <button onClick={() => navigate("/projects")}>
         View Projects
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,40 +1,74 @@
 import "./Skills.css";
-import { FaPython, FaReact, FaJs, FaHtml5, FaCss3Alt, FaDatabase } from "react-icons/fa";
+import { FaPython, FaReact, FaHtml5, FaCss3Alt, FaNodeJs, FaGithub, FaGit} from "react-icons/fa";
+import { SiDjango, SiJavascript, SiAngular } from "react-icons/si";
+import { SiMysql, SiPostgresql, SiMongodb, SiSqlite, SiPostman, SiExpress } from "react-icons/si";
+import { TbApi } from "react-icons/tb";
 
-function Skills() {
-  const skills = [
-    { name: "Python", icon: <FaPython />, level: "90%" },
-    { name: "Django", icon: <FaDatabase />, level: "85%" },
-    { name: "React", icon: <FaReact />, level: "80%" },
-    { name: "JavaScript", icon: <FaJs />, level: "85%" },
-    { name: "HTML", icon: <FaHtml5 />, level: "95%" },
-    { name: "CSS", icon: <FaCss3Alt />, level: "90%" }
-  ];
+const skillsData = [
+  {
+    title: "Backend",
+    items: [
+      { name: "Python", icon:  <FaPython />, level: "85%" },
+      { name: "Django", icon: <SiDjango />, level: "80%" },
+      { name: "Node.js", icon: <FaNodeJs />, level: "70%" },
+      { name: "REST API", icon: <TbApi />, level: "75%" },
+    ],
+  },
+  {
+    title: "Frontend",
+    items: [
+      { name: "React", icon: <FaReact />, level: "80%" },
+      { name: "JavaScript", icon: <SiJavascript />, level: "85%" },
+      { name: "Angular", icon: <SiAngular />, level: "70%" },
+      { name: "HTML", icon: <FaHtml5 />, level: "85%" },
+      { name: "CSS", icon: <FaCss3Alt />, level: "80%" },
+    ],
+  },
+  {
+    title: "Database",
+    items: [
+      { name: "MySQL", icon: <SiMysql />, level: "85%" },
+      { name: "PostgreSQL", icon: <SiPostgresql />, level: "70%" },
+      { name: "MongoDB", icon: <SiMongodb />, level: "80%" },
+      { name: "SQLite", icon: <SiSqlite />, level: "90%" },
+    ]
+  },
+  {
+    title: "Tools",
+    items: [
+      { name: "Git", icon: <FaGit />, level: "75%" },
+      { name: "GitHub", icon: <FaGithub />, level: "80%" },
+      { name: "Postman", icon: <SiPostman />, level: "70%" },
+    ],
+  },
+];
 
+export default function Skills() {
   return (
-    <section data-aos="fade-right" className="skills-section">
-     <div className="glass-card">
-      <h1 className="title">Skills</h1>
+    <section className="skills-section">
+      <h2>Skills</h2>
 
-      <div className="skills-grid">
-        {skills.map((skill, i) => (
-          <div key={i} className="card">
-            <div className="icon">{skill.icon}</div>
-            <p>{skill.name}</p>
+      {skillsData.map((category, index) => (
+        <div key={index} className="category">
+          <h3 className="category-title">{category.title}</h3>
 
-            {/* Skill Bar */}
-            <div className="skill-bar">
-              <div
-                className="skill-fill"
-                style={{ width: skill.level }}
-              ></div>
-            </div>
+          <div className="skills-grid">
+            {category.items.map((skill, i) => (
+              <div key={i} className="card">
+                <div className="icon">{skill.icon}</div>
+                <h4>{skill.name}</h4>
+
+                <div className="skill-bar">
+                  <div
+                    className="skill-fill"
+                    style={{ width: skill.level }}
+                  ></div>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-     </div>
+        </div>
+      ))}
     </section>
   );
 }
-
-export default Skills;
